cedc48fb064752fd928c1d12aeef9eeaeda79c10
Neerajakshulu Ambati <neeraj.ambati@metavaratechnologies.com>
2026-07-17 11:23:59 +0530

    Revert "Dashbord, Home, Layout pages implemend"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
-import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import DashboardHome from './Dashboard';
-import AdminDashboard from './MetaNestHome/Admin/AdminDashboard';
-import ResidentDashboard from './MetaNestHome/Resident/ResidentDashboard';
-import LayoutDashboard from './MetaNestLayout/LayoutDashboard';
 
 function App() {
-  const navigate = useNavigate();
-
-  return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="/home" element={<DashboardHome />} />
-      <Route path="/dashboard" element={<DashboardHome />} />
-      <Route path="/home/admin" element={<AdminDashboard onBack={() => navigate('/home')} />} />
-      <Route
-        path="/home/resident"
-        element={<ResidentDashboard onBack={() => navigate('/home')} />}
-      />
-      <Route path="/layout" element={<LayoutDashboard onBack={() => navigate('/home')} />} />
-      <Route path="*" element={<Navigate to="/home" replace />} />
-    </Routes>
-  );
+  return <DashboardHome />;
 }
 
 export default App;
